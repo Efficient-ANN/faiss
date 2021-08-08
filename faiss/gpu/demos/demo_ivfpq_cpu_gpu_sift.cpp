107b7e93ba4a8fc0ab88a46f14893f2615642d8c
Alan Prado <alanpradoaraujo@gmail.com>
2021-08-08 19:45:00 -0300

    fix(faiss/gpu/demos): fix maximum tile size for adding in IVFPQ with CPU and GPU

--- faiss/gpu/demos/demo_ivfpq_cpu_gpu_sift.cpp
+++ faiss/gpu/demos/demo_ivfpq_cpu_gpu_sift.cpp
@@ -271,6 +271,7 @@ void demo_ivfpq(int d, int coarseCodebookSize, int numSubQuantizers,
       size_t maxAddTileSize = (size_t)8 * 1024 * 1024 * 1024;
       size_t numVecsTile = maxAddTileSize / (d * sizeof(float));
       numVecsTile = std::min(numVecsTile, numIndexingVecs);
+      numVecsTile = std::min(numVecsTile, (size_t)10000);
       numVecsTile = std::max(numVecsTile, (size_t)1);
       tStart = clock();
       for (size_t i = 0; i < numIndexingVecs; i += numVecsTile) {
@@ -310,6 +311,7 @@ void demo_ivfpq(int d, int coarseCodebookSize, int numSubQuantizers,
       size_t maxAddTileSize = (size_t)8 * 1024 * 1024 * 1024;
       size_t numVecsTile = maxAddTileSize / (d * sizeof(float));
       numVecsTile = std::min(numVecsTile, numIndexingVecs);
+      numVecsTile = std::min(numVecsTile, (size_t)10000);
       numVecsTile = std::max(numVecsTile, (size_t)1);
       tStart = clock();
       for (size_t i = 0; i < numIndexingVecs; i += numVecsTile) {
