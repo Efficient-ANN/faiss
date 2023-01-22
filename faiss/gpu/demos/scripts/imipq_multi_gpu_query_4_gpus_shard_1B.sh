bb9368937f0fd45ea1b8154b83b72b871f92d093
AlaoPrado <alanpradoaraujo@gmail.com>
2023-01-22 16:11:39 -0300

    feat(faiss/gpu/demos/scripts): include scripts for demos with multiple gpus

--- faiss/gpu/demos/scripts/imipq_multi_gpu_query_4_gpus_shard_1B.sh
+++ faiss/gpu/demos/scripts/imipq_multi_gpu_query_4_gpus_shard_1B.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH -p ict_gpu       #Fila (partition) a ser utilizada
-#SBATCH --time=2:00:00
+#SBATCH --time=10:00:00
 #SBATCH --account=petrobrasiageo
 #SBATCH --exclusive         #Utilização exclusiva dos nós
 #SBATCH --job-name="a1test"
-
-
-
-# Num. GPUs: 1
-# Tempo esperado: 2:00:00
 
 module load boost/1.73_gnu cmake/3.17.3 gcc/10.2
 
@@ -19,5 +14,4 @@ BIGANN_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/gab/pqnns-m
 RESULT_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/demo_out
 DEMO_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/build/faiss/gpu/demos
 
-./${DEMO_DIR}/demo_imipq_gpu_sift_m 128 15560 8 8 ${BIGANN_DIR}/bigann_learn.bvecs 32381696 ${BIGANN_DIR}/bigann_base.bvecs 125000000 ${BIGANN_DIR}/bigann_base.bvecs 0 "" 4 14 0 12 0 11 0 1 1 1 28991029248 ${RESULT_DIR}/coarse/coarse_imipq15560_32M_m_replica.bin ${RESULT_DIR}/index/index_imipq15560_32M_m_replica_125M.bin 0 0 &> ${RESULT_DIR}/outs/1_gpus_imipq1000M_15560_32M_m_shard_27GB_1_gpu_125M.txt
-
+${DEMO_DIR}/demo_imipq_gpu_sift_m 128 15560 8 8 ${BIGANN_DIR}/bigann_learn.bvecs 32381696 ${BIGANN_DIR}/bigann_base.bvecs 1000000000 ${BIGANN_DIR}/bigann_base.bvecs 0 "" 4 14 0 12 0 11 0 4 4 1 28991029248 ${RESULT_DIR}/coarse/coarse_imipq15560_32M_m_replica.bin ${RESULT_DIR}/index/index_imipq15560_32M_m_replica.bin 1 0 &> ${RESULT_DIR}/outs/1_gpus_imipq1000M_15560_32M_m_shard_27GB_4_gpu_1000M.txt
--- faiss/gpu/demos/scripts/imipq_single_gpu_create_index_250M.sh
+++ faiss/gpu/demos/scripts/imipq_single_gpu_create_index_250M.sh
@@ -19,5 +19,5 @@ BIGANN_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/gab/pqnns-m
 RESULT_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/demo_out
 DEMO_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/build/faiss/gpu/demos
 
-./${DEMO_DIR}/demo_imipq_gpu_sift_m 128 15560 8 8 ${BIGANN_DIR}/bigann_learn.bvecs 32381696 ${BIGANN_DIR}/bigann_base.bvecs 125000000 ${BIGANN_DIR}/bigann_base.bvecs 0 "" 4 14 0 12 0 11 0 1 1 1 28991029248 ${RESULT_DIR}/coarse/coarse_imipq15560_32M_m_replica.bin ${RESULT_DIR}/index/index_imipq15560_32M_m_replica_125M.bin 0 0 &> ${RESULT_DIR}/outs/1_gpus_imipq1000M_15560_32M_m_shard_27GB_1_gpu_125M.txt
+./${DEMO_DIR}/demo_imipq_gpu_sift_m 128 15560 8 8 ${BIGANN_DIR}/bigann_learn.bvecs 32381696 ${BIGANN_DIR}/bigann_base.bvecs 250000000 ${BIGANN_DIR}/bigann_base.bvecs 0 "" 4 14 0 12 0 11 0 1 1 1 28991029248 ${RESULT_DIR}/coarse/coarse_imipq15560_32M_m_replica.bin ${RESULT_DIR}/index/index_imipq15560_32M_m_replica_250M.bin 0 0 &> ${RESULT_DIR}/outs/1_gpus_imipq1000M_15560_32M_m_shard_27GB_1_gpu_250M.txt 
 
