b83ba4a41c8300581bb393f90194db25da191a73
AlaoPrado <alanpradoaraujo@gmail.com>
2023-09-06 20:57:30 -0300

    feat(faiss/gpu/demos/scripts): create scripts for profiling IMIPQ in 2 gpus using nvprof with last argument configuration

---
+++ faiss/gpu/demos/scripts/imipq_multi_gpu_query_2_gpus_shard_500M_debugPrints_prof.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH -p ict_gpu       #Fila (partition) a ser utilizada
+#SBATCH --time=3:00:00
+#SBATCH --account=petrobrasiageo
+#SBATCH --exclusive         #Utilização exclusiva dos nós
+#SBATCH --job-name="a1test"
+
+module load boost/1.73_gnu cmake/3.17.3 gcc/10.2 cuda/11.8 gcc/7.4
+module load /scratch/app/modulos/sequana/current openmpi/gnu/4.0.1_sequana
+
+set -x
+
+BIGANN_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/gab/pqnns-multi-stream/data/sift1bi
+RESULT_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/demo_out
+DEMO_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/build/faiss/gpu/demos
+
+nvprof --print-gpu-trace ${DEMO_DIR}/demo_imipq_gpu_sift_m 128 15560 8 8 ${BIGANN_DIR}/bigann_learn.bvecs 32381696 ${BIGANN_DIR}/bigann_base.bvecs 500000000 ${BIGANN_DIR}/bigann_base.bvecs 0 "" 9 14 6 7 6 8 0 48 2 1 28991029248 ${RESULT_DIR}/coarse/coarse_imipq15560_32M_m_replica.bin ${RESULT_DIR}/index/index_imipq15560_32M_m_replica_500M.bin 1 0 0 1 1 | tee ${RESULT_DIR}/outs/1_gpus_imipq1000M_15560_32M_m_shard_27GB_2_gpu_500M_debugPrints_prof.txt
