28ae200fa802fccc03c563dc6c123a6efd6cec76
AlaoPrado <alanpradoaraujo@gmail.com>
2023-09-24 19:54:43 -0300

    feat(faiss/gpu/demos): include basic demo for testing CUDA and returning available memory

---
+++ faiss/gpu/demos/demo_memory.cpp
@@ -0,0 +1,12 @@
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+
+int main(int argc, char const *argv[]) {
+    size_t devFree = 0;
+    size_t devTotal = 0;
+    cudaMemGetInfo(&devFree, &devTotal);
+    std::cout << "-------Memory-------" << std::endl;
+    std::cout << "Free: " << devFree << std::endl;
+    std::cout << "Total: " << devTotal << std::endl;
+    return 0;
+}
