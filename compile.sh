916e7d23b7c910fa3ea64d7875e09fd353276807
AlaoPrado <alanpradoaraujo@gmail.com>
2023-10-08 12:19:57 -0300

    feat: improving compilation script; include some scripts for testing different linking approaches

--- compile.sh
+++ compile.sh
@@ -1,12 +1,15 @@
-module load boost/1.73_gnu cmake/3.17.3 gcc/10.2 cuda/11.8 gcc/7.4
+module load boost/1.73_gnu cmake/3.17.3 gcc/10.2 cuda/11.4 gcc/7.4
 module load /scratch/app/modulos/sequana/current openmpi/gnu/4.0.1_sequana
 
 cd build
-cmake --build .
+cmake -B build . -DFAISS_ENABLE_PYTHON=OFF -DCMAKE_CUDA_ARCHITECTURES="35;50;52;60;61;70;75"
+make -C build -j8
 
 cd faiss/gpu/demos
 make demo_imipq_gpu_sift_m -j8
+make demo_memory -j8
+make demo_vector_residual -j8
+make demo_imipq_gpu_sift_m_v2 -j8
+make demo_memory_v2 -j8
 
-cd ../../../
-make -j8
 
---
+++ faiss/gpu/demos/scripts/runDemoMemory_v2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH -p ict_gpu       #Fila (partition) a ser utilizada
+#SBATCH --time=0:02:00
+#SBATCH --account=petrobrasiageo
+#SBATCH --exclusive         #Utilização exclusiva dos nós
+#SBATCH --job-name="a1test"
+
+module load boost/1.73_gnu cmake/3.17.3 gcc/10.2 gcc/7.4 nvhpc/22.3
+#module load cuda/11.8
+module load cuda/11.4
+module load /scratch/app/modulos/sequana/current openmpi/gnu/4.0.1_sequana
+
+set -x
+
+echo $CUDA_HOME
+
+ls $CUDA_HOME
+
+nvprof --version
+
+BIGANN_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/gab/pqnns-multi-stream/data/sift1bi
+RESULT_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/demo_out
+DEMO_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/build/faiss/gpu/demos
+
+nvprof --print-gpu-trace ${DEMO_DIR}/demo_memory_v2
