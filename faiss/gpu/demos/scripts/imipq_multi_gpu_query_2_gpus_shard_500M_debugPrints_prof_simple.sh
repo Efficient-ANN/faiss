a0688e046d727db86a2086f0191b20df736f6e17
Willian <guns945@gmail.com>
2023-10-16 12:50:50 -0300

    .

--- faiss/gpu/demos/scripts/imipq_multi_gpu_query_2_gpus_shard_500M_debugPrints_prof_simple.sh
+++ faiss/gpu/demos/scripts/imipq_multi_gpu_query_2_gpus_shard_500M_debugPrints_prof_simple.sh
@@ -7,8 +7,7 @@
 #SBATCH --job-name="a1test"
 
 module load boost/1.73_gnu cmake/3.17.3 gcc/10.2 gcc/7.4 nvhpc/22.3
-#module load cuda/11.8
-module load cuda/11.4
+module load cuda/11.0
 module load /scratch/app/modulos/sequana/current openmpi/gnu/4.0.1_sequana
 
 set -x
@@ -19,9 +18,9 @@ ls $CUDA_HOME
 
 nvprof --version
 
-BIGANN_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/gab/pqnns-multi-stream/data/sift1bi
-RESULT_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/demo_out
-DEMO_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/alan/faiss_imipq/build/faiss/gpu/demos
+BIGANN_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/g/pqnns-multi-stream/data/sift1bi
+RESULT_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/a/faiss_imipq/demo_out
+DEMO_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/a/faiss_imipq/build/faiss/gpu/demos
 
 ${DEMO_DIR}/demo_imipq_gpu_sift_m 128 16 8 8 ${BIGANN_DIR}/bigann_learn.bvecs 128 ${BIGANN_DIR}/bigann_base.bvecs 624 ${BIGANN_DIR}/bigann_base.bvecs 0 "" 0 1 6 7 6 7 0 8 2 1 4194304 ${RESULT_DIR}/coarse/coarse_imipq16_128_m_replica.bin ${RESULT_DIR}/index/index_imipq16_128_m_replica_624.bin 0 0 0 1 1 | tee ${RESULT_DIR}/outs/1_gpus_imipq624_16_128_m_shard_4MB_2_gpu_128_debugPrints_prof_simple.txt
 
