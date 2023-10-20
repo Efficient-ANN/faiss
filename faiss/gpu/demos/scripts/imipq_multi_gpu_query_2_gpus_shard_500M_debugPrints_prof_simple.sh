61b0c9e917f01be648e5326275acdef9e9359561
Willian <guns945@gmail.com>
2023-10-20 19:49:12 -0300

    .

--- faiss/gpu/demos/scripts/imipq_multi_gpu_query_2_gpus_shard_500M_debugPrints_prof_simple.sh
+++ faiss/gpu/demos/scripts/imipq_multi_gpu_query_2_gpus_shard_500M_debugPrints_prof_simple.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH -p ict_gpu       #Fila (partition) a ser utilizada
-#SBATCH --time=0:01:00
+#SBATCH --time=1:00:00
 #SBATCH --account=petrobrasiageo
 #SBATCH --exclusive         #Utilização exclusiva dos nós
 #SBATCH --job-name="a1test"
