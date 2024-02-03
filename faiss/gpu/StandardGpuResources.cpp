39f5178233d1c899e94ecbfcd458f01492d5089d
Alan Prado <alanpradoaraujo@gmail.com>
2024-02-03 20:05:37 -0300

    fix: remove debugging prints

--- faiss/gpu/StandardGpuResources.cpp
+++ faiss/gpu/StandardGpuResources.cpp
@@ -365,13 +365,11 @@ void StandardGpuResourcesImpl::initializeForDevice(int device) {
 }
 
 cublasHandle_t StandardGpuResourcesImpl::getBlasHandle(int device) {
-  std::cout << "MYGPU: getBlasHandle" << device << std::endl;
   initializeForDevice(device);
   return blasHandles_[device];
 }
 
 cudaStream_t StandardGpuResourcesImpl::getDefaultStream(int device) {
-  std::cout << "MYGPU: getDefaultStream" << device << std::endl;
   initializeForDevice(device);
 
   auto it = userDefaultStreams_.find(device);
@@ -386,7 +384,6 @@ cudaStream_t StandardGpuResourcesImpl::getDefaultStream(int device) {
 
 std::vector<cudaStream_t>
 StandardGpuResourcesImpl::getAlternateStreams(int device) {
-  std::cout << "MYGPU: getAlternateStreams" << device << std::endl;
   initializeForDevice(device);
   return alternateStreams_[device];
 }
@@ -396,13 +393,11 @@ std::pair<void *, size_t> StandardGpuResourcesImpl::getPinnedMemory() {
 }
 
 cudaStream_t StandardGpuResourcesImpl::getAsyncCopyStream(int device) {
-  std::cout << "MYGPU: getAsyncCopyStream" << device << std::endl;
   initializeForDevice(device);
   return asyncCopyStreams_[device];
 }
 
 void *StandardGpuResourcesImpl::allocMemory(const AllocRequest &req) {
-   std::cout << "MYGPU: allocMemory" << req.device << std::endl;
   initializeForDevice(req.device);
 
   // We don't allocate a placeholder for zero-sized allocations
