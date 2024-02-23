15dc86b4885f470ec153d784005e298125484b4c
Alan Prado <alanpradoaraujo@gmail.com>
2024-02-22 21:27:52 -0300

    fix(faiss/gpu/demos/scripts/mpi/sift): fix nvprof wrapper for empty strings

--- faiss/gpu/demos/scripts/mpi/sift/nvprof_wrapper.sh
+++ faiss/gpu/demos/scripts/mpi/sift/nvprof_wrapper.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-nvprof --print-gpu-trace -o $1.$OMPI_COMM_WORLD_RANK.nvprof ${@:2}
+argList=()
+i=1
+for var in "${@:2}"
+do
+    if [[ -z $var ]]; then
+        echo "$i = \"\""
+        argList+=("")
+    else
+        echo "$i = $var"
+        argList+=($var)
+    fi
+    i=$((i+1))
+done
+
+nvprof --print-gpu-trace -o $1.$OMPI_COMM_WORLD_RANK.nvprof ${argList[@]+"${argList[@]}"}
