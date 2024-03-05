c00f486bb1fe806ce82d30f7f98cf7e68126ac60
Alan Prado <alanpradoaraujo@gmail.com>
2024-03-04 21:10:24 -0300

    fix(faiss/gpu/demos/scripts/mpi/gist): include modules

--- faiss/gpu/demos/scripts/mpi/gist/gist_multi_gpu.sh
+++ faiss/gpu/demos/scripts/mpi/gist/gist_multi_gpu.sh
@@ -6,10 +6,10 @@
 #SBATCH --exclusive         #Utilização exclusiva dos nós
 #SBATCH --job-name="a1test"
 
-# module load boost/1.73_gnu cmake/3.17.3 gcc/10.2 gcc/7.4 
-# module load cuda/11.0
-# module load /scratch/app/modulos/sequana/current openmpi/gnu/4.0.1_sequana
-# set -x
+module load boost/1.73_gnu cmake/3.17.3 gcc/10.2 gcc/7.4 
+module load cuda/11.0
+module load /scratch/app/modulos/sequana/current openmpi/gnu/4.0.1_sequana
+set -x
 
 # FAISS_DIR=/home/alanp/git/faiss_imipq
 FAISS_DIR=/petrobr/parceirosbr/petrobrasiageo/willian.barreiros/git/a/faiss_imipq
